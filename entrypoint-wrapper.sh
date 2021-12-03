aee9561a4795ae50ce2c247509e0ee9831eed28f
raju7258 <rohanmtoo@gmail.com>
2021-12-03 19:40:49 +0400

    Update entrypoint-wrapper.sh

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p ./content/adapters/storage
 cp -r ./node_modules/ghost-storage-adapter-s3/ ./content/adapters/storage/s3
-chmod 600 /usr/local/bin/docker-entrypoint.sh
-sh /usr/local/bin/docker-entrypoint.sh
+chmod +x /usr/local/bin/docker-entrypoint.sh
+./usr/local/bin/docker-entrypoint.sh
